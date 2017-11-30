fdaa48ecfeb31879813ec4bca1e22daa5c98cd8f
your name <your_email@youremail.com>
2017-11-30 11:20:30 +0800

    nothing

--- cloud-config-client/src/main/java/com/jay/cloud/CloudConfigClientApplication.java
+++ cloud-config-client/src/main/java/com/jay/cloud/CloudConfigClientApplication.java
@@ -2,7 +2,18 @@ package com.jay.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.bus.BusAutoConfiguration;
+import org.springframework.cloud.bus.endpoint.AbstractBusEndpoint;
+import org.springframework.cloud.bus.endpoint.BusEndpoint;
+import org.springframework.cloud.bus.event.EnvironmentChangeListener;
+import org.springframework.cloud.bus.event.RefreshListener;
+import org.springframework.cloud.bus.event.RefreshRemoteApplicationEvent;
+import org.springframework.cloud.bus.event.SentApplicationEvent;
+import org.springframework.cloud.context.refresh.ContextRefresher;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.SimpleApplicationEventMulticaster;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -10,5 +21,6 @@ public class CloudConfigClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudConfigClientApplication.class, args);
+
 	}
 }
--- cloud-config-server/src/main/java/com/jay/cloud/CloudConfigServerApplication.java
+++ cloud-config-server/src/main/java/com/jay/cloud/CloudConfigServerApplication.java
@@ -2,6 +2,9 @@ package com.jay.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.bus.endpoint.RefreshBusEndpoint;
+import org.springframework.cloud.bus.event.EnvironmentChangeListener;
+import org.springframework.cloud.bus.event.RefreshListener;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.ApplicationContext;
